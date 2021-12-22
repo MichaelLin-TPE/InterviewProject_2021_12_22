53119103b053d2961f324307f9162cb959f734a0
Michael <m24572582@gmail.com>
2021-12-23 00:42:52 +0800

    Android Commit : 增加Presenter 註解

--- app/src/main/java/com/money/a2021_12_22_interview_project/page2/Page2Presenter.kt
+++ app/src/main/java/com/money/a2021_12_22_interview_project/page2/Page2Presenter.kt
@@ -1,8 +1,23 @@
 package com.money.a2021_12_22_interview_project.page2
 
 interface Page2Presenter {
+    /**
+     * 程式起始點
+     */
     fun onActivityStart(login: String)
+
+    /**
+     * Activity Destroy 時要做的事情
+     */
     fun onActivityDestroy()
+
+    /**
+     * 點擊BACK ICON
+     */
     fun onBackButtonClickListener()
+
+    /**
+     * 點擊BlogURL
+     */
     fun onBlogUrlClickListener(url: String)
 }
